5ef6fdc0d6189b9c2f91196177a3c2e07824da9a
Vishal Singh <vishal.singh@caastle.com>
2020-01-15 02:15:20 +0530

    extract branch name and append commit message to it

--- Sources/CommitWithBranchNameCore/CommitWithBranchName.swift
+++ Sources/CommitWithBranchNameCore/CommitWithBranchName.swift
@@ -7,7 +7,21 @@ public final class CommitWithBranchName {
         self.arguments = arguments
     }
 
+    //git rev-parse --abbrev-ref HEAD
+
+    
     public func run() throws {
-        print("Hello world")
+        let bash = Bash()
+        guard let branchName = bash.execute(commandName: "git", arguments: ["rev-parse", "--abbrev-ref", "HEAD"])?.trimmingCharacters(in: NSCharacterSet.newlines) else {
+            return
+        }
+        if CommandLine.argc < 2 {
+            print("The commit message is: \(branchName) no message")
+        } else {
+            print("Arguments are passed.")
+            var message = CommandLine.arguments
+            message.remove(at: 0)
+            print("The commit message is: \(branchName) \(message.joined(separator: " "))")
+        }
     }
 }
