3a4e9a6d7afec0f01dd546af67b6727b3e106cd8
Vishal Singh <vishal.singh@caastle.com>
2020-01-14 18:19:28 +0530

    create a dependency as package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "CommitWithBranchName",
-    dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommitWithBranchName",
-            dependencies: []),
-        .testTarget(
-            name: "CommitWithBranchNameTests",
-            dependencies: ["CommitWithBranchName"]),
+            dependencies: ["CommitWithBranchNameCore"]),
+        .target(name: "CommitWithBranchNameCore")
     ]
 )
--- Sources/CommitWithBranchName/main.swift
+++ Sources/CommitWithBranchName/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import CommitWithBranchNameCore
+
+let tool = CommitWithBranchName()
+
+do {
+    try tool.run()
+} catch {
+    print("Whoops! An error occurred: \(error)")
+}
