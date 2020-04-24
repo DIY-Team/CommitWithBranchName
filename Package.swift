35042342aca50ddfb0a84cc91f026df5b78b78c8
Vishal Singh <vishalsingh2706@gmail.com>
2020-04-24 07:23:49 +0530

    update package description

--- Package.swift
+++ Package.swift
@@ -6,13 +6,17 @@ import PackageDescription
 let package = Package(
     name: "CommitWithBranchName",
     dependencies: [
-    .package(url: "https://github.com/DIY-Team/RunsShellCommand", from: "0.0.1")
+        .package(url: "https://github.com/DIY-Team/install-swift-script.git", from: "0.0.1"),
+        .package(url: "https://github.com/DIY-Team/RunsShellCommand", from: "0.0.1")
     ],
     targets: [
         .target(
             name: "CommitWithBranchName",
             dependencies: ["CommitWithBranchNameCore"]),
         .target(name: "CommitWithBranchNameCore",
-            dependencies: ["RunsShellCommand"])
+                dependencies: ["RunsShellCommand"]),
+        .target(
+            name: "Install",
+            dependencies: ["InstallSwiftScriptCore"])
     ]
 )
